77601d50305dcc2e291f51dd0034327ab8ea8fb7
Carlos Alberto Gonçalves Simoes <carlos.simoes@compasso.com.br>
2025-04-29 18:19:04 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ 04.rest-with-spring-boot-and-java-erudio/src/main/java/com/github/cgsimoes23/Startup.java
@@ -0,0 +1,14 @@
+package com.github.cgsimoes23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ 04.rest-with-spring-boot-and-java-erudio/src/main/java/com/github/cgsimoes23/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.github.cgsimoes23.model;
+
+public record Greeting(Long id, String content) {
+}
