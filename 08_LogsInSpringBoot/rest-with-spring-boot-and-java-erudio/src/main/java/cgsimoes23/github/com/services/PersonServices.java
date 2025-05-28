108d343ab4c88ead1d2e1227715b13cd66446ab1
unknown <cgsimoes23@gmail.com>
2025-05-28 19:54:29 -0300

    Licao 8

---
+++ 08_LogsInSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/cgsimoes23/github/com/services/PersonServices.java
@@ -0,0 +1,65 @@
+package cgsimoes23.github.com.services;
+
+import cgsimoes23.github.com.controllers.PersonController;
+import cgsimoes23.github.com.exception.ResourceNotFoundException;
+import cgsimoes23.github.com.model.Person;
+import cgsimoes23.github.com.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service // This annotation indicates that this class is a service component in the Spring framework.
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonController.class.getName());
+
+    @Autowired
+    PersonRepository repository;
+
+    public List<Person> findAll() {
+
+        logger.info("Finding all people!");
+
+        return repository.findAll();
+    }
+
+    public Person findById(Long id) {
+        logger.info("Finding one person!");
+
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(   "Person not found with id: " + id));
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one person!");
+
+        return repository.save(person);
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one person!");
+
+        Person entity = repository.findById(person.getId())
+                        .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return repository.save(person);
+    }
+
+    public void delete(Long id) {
+        logger.info("Deleting one person!");
+        Person entity = repository.findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        repository.delete(entity);
+    }
+
+}
