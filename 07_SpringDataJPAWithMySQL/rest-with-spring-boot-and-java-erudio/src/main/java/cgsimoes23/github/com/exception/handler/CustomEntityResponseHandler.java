b1cd3b349ebb60be272da6cfa2ba0fe1a0895bda
unknown <cgsimoes23@gmail.com>
2025-05-28 19:47:51 -0300

    Licao 7

--- 07_SpringDataJPAWithMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/cgsimoes23/github/com/exception/handler/CustomEntityResponseHandler.java
+++ 07_SpringDataJPAWithMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/cgsimoes23/github/com/exception/handler/CustomEntityResponseHandler.java
@@ -1,7 +1,7 @@
 package cgsimoes23.github.com.exception.handler;
 
 import cgsimoes23.github.com.exception.ExceptionResponse;
-import cgsimoes23.github.com.exception.UnsupportedMathOperationException;
+import cgsimoes23.github.com.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -26,12 +26,12 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(Exception ex, WebRequest request) {
         ExceptionResponse response = new ExceptionResponse(
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
