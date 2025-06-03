806843846b5fa10086dfac678f721e4fbe80364f
unknown <cgsimoes23@gmail.com>
2025-06-03 14:38:14 -0300

    Licao 8

--- 09_WorkingWithDTOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/cgsimoes23/github/com/exception/ExceptionResponse.java
+++
@@ -1,7 +0,0 @@
-package cgsimoes23.github.com.exception;
-
-import java.util.Date;
-
-public record ExceptionResponse(Date timestamp, String message, String details) {
-
-}
--- 09_WorkingWithDTOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/cgsimoes23/github/com/repository/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package cgsimoes23.github.com.repository;
-
-import cgsimoes23.github.com.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {}
